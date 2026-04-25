ac8ce0953cdc2cbf18173784e564e9b97e276ee2
prithika1207 <prithikapradhaban0@gmail.com>
2026-04-25 14:42:46 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,9 +1,9 @@
 `default_nettype none
 
-module tt_um_down_counter (
-    input  wire [7:0] ui_in,     // not used
+module tt_um_downcounter (
+    input  wire [7:0] ui_in,     // unused
     output wire [7:0] uo_out,    // counter output
-    input  wire [7:0] uio_in,    // not used
+    input  wire [7:0] uio_in,    // unused
     output wire [7:0] uio_out,
     output wire [7:0] uio_oe,
     input  wire       ena,
@@ -15,14 +15,14 @@ module tt_um_down_counter (
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
-            count <= 8'd0;        // reset to 0
+            count <= 8'd0;        // reset
         else if (ena)
-            count <= count - 1;   // DOWN counter
+            count <= count - 1;   // down counter
     end
 
     assign uo_out = count;
 
-    assign uio_out = 8'b0;
-    assign uio_oe  = 8'b0;
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
 
 endmodule
